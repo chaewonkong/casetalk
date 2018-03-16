1a6fc5b6a0f9f65b917b5407842cc62a3eebb728
공채원 <chaewonkong@gmail.com>
2018-03-16 09:41:09 +0900

    Add keyboard()

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def keyboard(request):
+
+	return JsonResponse({})
+
+
+def message(request):
+
+	return JsonResponse({
+		"message": {
+			"text": "판례번호를 띄어쓰기 없이 입력해주세요"
+		},
+		})
