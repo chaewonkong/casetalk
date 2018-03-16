56420b07ee4bed11eadcd5ee2bebd59887dca704
공채원 <chaewonkong@gmail.com>
2018-03-16 09:50:37 +0900

    Add message()

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from api.scraper import case_scraper
 
 
 def keyboard(request):
 
-	return JsonResponse({})
+	return JsonResponse({
+		"message":{"text": "판례번호를 띄어쓰기 없이 입력해주세요"},
+		})
 
 
 def message(request):
 
-	return JsonResponse({
-		"message": {
-			"text": "판례번호를 띄어쓰기 없이 입력해주세요"
-		},
-		})
+	case = case_scraper(request)
+
+	if case:
+		return JsonResponse({
+			"message": {
+				"text": request + "\n\n판시사항\n\n" + case[0] +
+						"\n\n판결요지\n\n" + case[1] +
+						"\n\n케이스노트에서 자세히 보기\n\n" + case[2]
+				},
+			})
+	else:
+		return JsonResponse({
+			"message": {"text": "판례번호를 띄어쓰기 없이 검색해주세요"}
+			})
