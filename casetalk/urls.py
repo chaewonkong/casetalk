73599b3ac29cd1c60d8936060cf2f5487ffa982a
공채원 <chaewonkong@gmail.com>
2018-03-16 09:57:50 +0900

    Add include to urls

--- casetalk/urls.py
+++ casetalk/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from chatbot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('keyboard/', 'chatbot.views.keyboard'),
-    path('message/', 'chatbot.views.message')
+    path('keyboard/', include('chatbot.views.keyboard')),
+    path('message/', include('chatbot.views.message')),
 ]
